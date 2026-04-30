fff10a501538f6a64e522b4ca0a6f94ec132695e
beimnetmelese <beimnetmelese16@gmail.com>
2026-04-30 20:16:28 +0300

    mysql migartions fixed

--- dipcom/__init__.py
+++ dipcom/__init__.py
@@ -1,3 +1,9 @@
 import pymysql
 
+# Django 5.2+ checks MySQLdb client version and expects >= 2.2.1.
+# When using PyMySQL as a drop-in replacement, expose compatible version
+# metadata before install_as_MySQLdb() so startup and migrations work.
+pymysql.version_info = (2, 2, 1, "final", 0)
+pymysql.__version__ = "2.2.1"
+
 pymysql.install_as_MySQLdb()
